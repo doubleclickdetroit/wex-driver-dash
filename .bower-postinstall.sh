056715c912772f2005ff88edc48feaa9f30f7c00
Ben Babics <bbabics@gmail.com>
2016-12-21 12:10:10 -0500

    added temp postinstall script for foundation-sites
    https://github.com/acoustep/ember-cli-foundation-6-sass/issues/40

---
+++ .bower-postinstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Foundation Sites move _vendor directory inside scss directory and update refs
+mv bower_components/foundation-sites/_vendor bower_components/foundation-sites/scss
+sed -i '' 's/\.\.\/_vendor/_vendor/g' bower_components/foundation-sites/scss/foundation.scss
