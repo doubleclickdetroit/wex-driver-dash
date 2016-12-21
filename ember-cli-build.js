373fe98b42250493872445f0ad2f114ce5f12391
Ben Babics <bbabics@gmail.com>
2016-12-21 13:50:57 -0500

    ember-cli-build import fontello css & fonts

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -23,5 +23,12 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
+  app.import( 'vendor/fontello/css/fontello.css' );
+  app.import( 'vendor/fontello/font/fontello.eot', { destDir: 'font' } );
+  app.import( 'vendor/fontello/font/fontello.woff2', { destDir: 'font' } );
+  app.import( 'vendor/fontello/font/fontello.woff', { destDir: 'font' } );
+  app.import( 'vendor/fontello/font/fontello.ttf', { destDir: 'font' } );
+  app.import( 'vendor/fontello/font/fontello.svg', { destDir: 'font' } );
+
   return app.toTree();
 };
