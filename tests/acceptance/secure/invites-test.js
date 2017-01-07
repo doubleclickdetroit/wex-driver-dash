2c517772ec4bd0e03afe19940727399886d8037f
Ben Babics <bbabics@gmail.com>
2017-01-06 20:08:01 -0500

    invites requests /drivers

---
+++ tests/acceptance/secure/invites-test.js
@@ -0,0 +1,13 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'driver-dash/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | secure/invites');
+
+test('visiting /secure/invites', function(assert) {
+  visit('/secure/invites');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/secure/invites');
+    assert.equal(find('.accordion-item:first .accordion-content tbody tr').length, 3);
+  });
+});
