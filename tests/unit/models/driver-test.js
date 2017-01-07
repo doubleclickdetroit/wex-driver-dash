00cb59016a559edcd54cfcdcde01040f381c8d90
Ben Babics <bbabics@gmail.com>
2017-01-06 20:07:27 -0500

    addition of driver model

---
+++ tests/unit/models/driver-test.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+import { moduleForModel, test } from 'ember-qunit';
+
+moduleForModel('driver', 'Unit | Model | driver', {
+  // Specify the other units that are required for this test.
+  needs: []
+});
+
+test('it exists', function(assert) {
+  let model = this.subject();
+  // let store = this.store();
+  assert.ok( !!model );
+});
+
+test('has a fullName computed property', function(assert) {
+  const model = this.subject(),
+        firstName = 'John',
+        lastName  = 'Doe';
+
+  Ember.run(() => {
+    model.setProperties({ firstName, lastName });
+    assert.equal( model.get('fullName'), `${firstName} ${lastName}` );
+  });
+});
