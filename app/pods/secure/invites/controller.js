396b19b25da4fda7b84ba4d13a4bef06267f51c1
Ben Babics <bbabics@gmail.com>
2017-01-11 09:56:48 -0500

    observe allValidItems, get dirty models and set isChecked

--- app/pods/secure/invites/controller.js
+++ app/pods/secure/invites/controller.js
@@ -2,19 +2,20 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   allValidItems: Ember.computed('model.@each.isValidPhone', function() {
-    return this.get( 'model' ).filter(item => {
-      const isValidPhone = item.get( 'isValidPhone' );
-      item.set( 'isChecked', isValidPhone );
-      return isValidPhone;
-    });
+    return this.get( 'model' ).filter( item => item.get('isValidPhone') );
   }),
 
   checkedItems: Ember.computed('model.@each.isChecked', function() {
     return this.get( 'model' ).filterBy( 'isChecked', true );
   }),
 
-  isAllChecked: Ember.computed('allValidItems', 'checkedItems', function() {
+  isAllChecked: Ember.computed('allValidItems', 'checkedItems.@each.isChecked', function() {
     return this.get( 'checkedItems.length' ) === this.get( 'allValidItems.length' );
+  }),
+
+  isValidPhoneDidChange: Ember.observer('allValidItems.@each.isPhoneDirty', function() {
+    const dirtyModels = this.get( 'allValidItems' ).filterBy( 'isPhoneDirty', true );
+    Ember.A( dirtyModels ).setEach( 'isChecked', true );
   }),
 
   actions: {
