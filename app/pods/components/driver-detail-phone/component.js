c0bcccc0207e7b2df479a2b69158005f60968af8
Ben Babics <bbabics@gmail.com>
2017-01-10 20:49:13 -0500

    input uses “buffer” instead of directly using “phone”

--- app/pods/components/driver-detail-phone/component.js
+++ app/pods/components/driver-detail-phone/component.js
@@ -1,12 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  phone:  '',
+  buffer: '',
+
   isEditable:   false,
   displayPhone: false,
 
-  phoneDidChange: Ember.on('init', Ember.observer('phone', function() {
-    this.set( 'displayPhone', !!this.get('phone') );
-  })),
+  initBuffer: Ember.on('init', function() {
+    const phone = this.get( 'phone' );
+
+    this.setProperties({
+      buffer: phone,
+      displayPhone: !!phone
+    });
+  }),
 
   actions: {
     handleAddPhone() {
@@ -15,16 +23,23 @@ export default Ember.Component.extend({
     },
     handleIsEditable(isEditable=true) {
       this.set( 'isEditable', isEditable );
+      Ember.run.later(() => { $( '.phone-input' ).focus(); }, 500);
     },
 
     handleLeaveField() {
       this.send( 'handleIsEditable', false );
-      if ( !this.get('phone') ) {
+
+      if ( !this.get('buffer') ) {
         this.set( 'displayPhone', false );
       }
     },
+    handleEnterKey() {
+      this.set( 'phone', this.get('buffer') );
+      this.send( 'handleLeaveField' );
+    },
     handleEscapeKey() {
       // rollback value
+      this.set( 'buffer', this.get('phone') );
       this.send( 'handleLeaveField' );
     }
   }
