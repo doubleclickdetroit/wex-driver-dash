69f9b7a9f5b8fd6ffd84c9ecb1d0b63e8020952b
Ben Babics <Ben.Babics@wexinc.com>
2017-01-12 14:20:10 -0500

    addition of tests for validate-phone

--- tests/unit/helpers/validate-phone-test.js
+++ tests/unit/helpers/validate-phone-test.js
@@ -4,9 +4,18 @@ import { module, test } from 'qunit';
 
 module('Unit | Helper | validate phone');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = validatePhone([42]);
-  assert.ok(result);
+test('it should validate on numbers', function(assert) {
+    assert.expect(4);
+    assert.ok( validatePhone('2075551212') );
+    assert.ok( validatePhone('207 555 1212') );
+    assert.ok( validatePhone('207.555.1212') );
+    assert.ok( validatePhone('(207) 555-1212') );
 });
 
+test('it should invalidate less than 10 numbers', function(assert) {
+    assert.expect(4);
+    assert.equal( validatePhone(), false );
+    assert.equal( validatePhone(null), false );
+    assert.equal( validatePhone(''), false );
+    assert.equal( validatePhone('123456789'), false );
+});
