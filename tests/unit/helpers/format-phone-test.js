1ad4d2d3f5426ccde041921be330f1b9e06947d2
Ben Babics <Ben.Babics@wexinc.com>
2017-01-12 14:20:40 -0500

    cleaned up test for format-phone

--- tests/unit/helpers/format-phone-test.js
+++ tests/unit/helpers/format-phone-test.js
@@ -4,18 +4,12 @@ import { module, test } from 'qunit';
 
 module('Unit | Helper | format phone');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = formatPhone([42]);
-  assert.ok(result);
-});
-
 test('it should format the phone number', function(assert) {
   const phone = '207-555-1212';
 
-    assert.expect(4);
-    assert.equal( formatPhone(['2075551212']), phone );
-    assert.equal( formatPhone(['207 555 1212']), phone );
-    assert.equal( formatPhone(['207.555.1212']), phone );
-    assert.equal( formatPhone(['(207) 555-1212']), phone );
+  assert.expect(4);
+  assert.equal( formatPhone('2075551212'), phone );
+  assert.equal( formatPhone('207 555 1212'), phone );
+  assert.equal( formatPhone('207.555.1212'), phone );
+  assert.equal( formatPhone('(207) 555-1212'), phone );
 });
