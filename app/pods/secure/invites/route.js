619794697ce9e0a70ebb9a5b0a372181db35a3ae
Ben Babics <Ben.Babics@wexinc.com>
2017-01-13 09:56:42 -0500

    remove clearing out phone of first driver

--- app/pods/secure/invites/route.js
+++ app/pods/secure/invites/route.js
@@ -2,12 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll( 'driver' )
-
-      // for the sake of developing adding a phone number
-      .then(drivers => {
-        drivers.get( 'firstObject' ).set( 'phone', '' );
-        return drivers;
-      });
+    return this.store.findAll( 'driver' );
   }
 });
