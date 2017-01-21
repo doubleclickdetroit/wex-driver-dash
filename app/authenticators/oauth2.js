9a72e5f409b0123d5089202a3be4c899a23ccaae
Ben Babics <bbabics@gmail.com>
2017-01-21 00:02:43 -0500

    authenticator accounts for grant_type and scope

--- app/authenticators/oauth2.js
+++ app/authenticators/oauth2.js
@@ -1,5 +1,15 @@
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
 
+const grant_type   = 'password';
+const scopeStrings = [ 'accounts', 'user:account_management', 'drivers' ];
+
 export default OAuth2PasswordGrant.extend({
-  serverTokenEndpoint: '/api/uaa/oauth/token'
+  serverTokenEndpoint: '/api/uaa/oauth/token',
+
+  authenticate(identification, password, scope=[], headers={}) {
+    headers.grant_type = grant_type;
+    scope.push( ...scopeStrings );
+
+    return this._super( identification, password, scope, headers );
+  }
 });
