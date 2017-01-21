c0230b739552a819d122f311edeec8b4cc3716c0
Ben Babics <bbabics@gmail.com>
2017-01-21 00:01:39 -0500

    login controller & template clean up

--- app/pods/login/controller.js
+++ app/pods/login/controller.js
@@ -10,11 +10,10 @@ export default Ember.Controller.extend({
   },
 
   actions: {
-    authenticate() {
-      let { identification, password } = this.getProperties( 'identification', 'password' );
-      this.get( 'session' ).authenticate('authenticator:oauth2', identification, password).catch(reason => {
-        this.set( 'errorMessage' , reason.error || reason );
-      });
+    handleAuthentication() {
+      let { session, identification, password } = this.getProperties( 'session', 'identification', 'password' );
+      session.authenticate( 'authenticator:oauth2', identification, password )
+        .catch(reason => { this.set( 'errorMessage' , reason.error || reason ); });
     }
   }
 });
