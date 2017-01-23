4f02e1ec3e6e44d6d06c3e3e6d071123a215c025
Ben Babics <Ben.Babics@wexinc.com>
2017-01-23 15:15:05 -0500

    remove grant_type as oauth has it in defaults

--- app/authenticators/oauth2.js
+++ app/authenticators/oauth2.js
@@ -1,16 +1,13 @@
 import ENV from 'driver-dash/config/environment';
 import OAuth2PasswordGrant from 'ember-simple-auth/authenticators/oauth2-password-grant';
 
-const grant_type   = 'password';
 const scopeStrings = [ 'accounts', 'user:account_management', 'drivers' ];
 
 export default OAuth2PasswordGrant.extend({
   serverTokenEndpoint: `${ENV.CONFIG.API.AUTH}${ENV.APP_CONFIG.ENDPOINTS.AUTH}`,
 
   authenticate(identification, password, scope=[], headers={}) {
-    headers.grant_type = grant_type;
     scope.push( ...scopeStrings );
-
     return this._super( identification, password, scope, headers );
   }
 });
