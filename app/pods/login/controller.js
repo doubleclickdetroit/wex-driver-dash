eb4b82673987e9e7c907d53f6b265705d386c3b4
Ben Babics <Ben.Babics@wexinc.com>
2017-01-24 16:58:18 -0500

    login disables button on pending, displays spinner
    # Conflicts:
    #	app/pods/login/controller.js

--- app/pods/login/controller.js
+++ app/pods/login/controller.js
@@ -1,19 +1,41 @@
 import Ember from 'ember';
+import { observes, on } from 'ember-computed-decorators';
 import computed from 'ember-computed-decorators';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service(),
+
+  isEnabled: false,
+  isPending: false,
 
   @computed( 'identification', 'password' )
   hasCredentials( identification, password ) {
     return !!identification && !!password;
   },
 
+  @on( 'init' )
+  @observes( 'isPending', 'hasCredentials' )
+  toggleIsEnabled() {
+    const isPending      = this.get( 'isPending' );
+    const hasCredentials = this.get( 'hasCredentials' );
+    this.set( 'isEnabled', !isPending && hasCredentials );
+  },
+
+  @observes( 'identification', 'password' )
+  dismissErrorMessage() {
+    if ( this.get('errorMessage') ) {
+      this.set( 'errorMessage', '' );
+    }
+  },
+
   actions: {
     handleAuthentication() {
       let { session, identification, password } = this.getProperties( 'session', 'identification', 'password' );
       session.authenticate( 'authenticator:oauth2', identification, password )
-        .catch(reason => { this.set( 'errorMessage' , reason.error ); });
+        .catch(reason => { this.set( 'errorMessage' , reason.error ); })
+        .finally(() => this.set( 'isPending', false ));
+
+      this.set( 'isPending', true );
     }
   }
 });
