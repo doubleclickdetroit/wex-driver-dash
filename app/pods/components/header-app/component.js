30cb6630c4ca9566a1131eb9069c5457b5022c87
Ben Babics <Ben.Babics@wexinc.com>
2017-01-25 16:01:06 -0500

    header-app render currentUser.company attrs

--- app/pods/components/header-app/component.js
+++ app/pods/components/header-app/component.js
@@ -1,10 +1,17 @@
 import Ember from 'ember';
+import { alias } from 'ember-computed-decorators';
 
 export default Ember.Component.extend({
   tagName:    'header',
   classNames: [ 'component-header-app' ],
 
   session: Ember.inject.service(),
+
+  @alias( 'session.currentUser' )
+  currentUser: null,
+
+  @alias( 'currentUser.company' )
+  company: null,
 
   actions: {
     invalidateSession() {
