ebc3e75c6390d45f902f5e37379912612a3d7692
Ben Babics <Ben.Babics@wexinc.com>
2017-01-26 16:55:05 -0500

    removed inverse looks up on user model, added onlineApplication property

--- app/models/user.js
+++ app/models/user.js
@@ -2,10 +2,11 @@ import DS from 'ember-data';
 const { attr, belongsTo } = DS;
 
 export default DS.Model.extend({
-  company:        belongsTo( 'account', { inverse: 'users' } ),
-  billingCompany: belongsTo( 'account', { inverse: 'usersBilling' } ),
+  company:        belongsTo( 'account' ),
+  billingCompany: belongsTo( 'account' ),
 
-  firstName: attr(),
-  email:     attr(),
-  username:  attr()
+  firstName:         attr(),
+  email:             attr(),
+  username:          attr(),
+  onlineApplication: attr()
 });
