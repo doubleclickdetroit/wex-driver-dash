529af812d561fb09d05a3a7ddb3fee3916ba04a1
Ben Babics <Ben.Babics@wexinc.com>
2017-01-27 15:28:28 -0500

    user & account serializer unit tests

--- tests/unit/serializers/account-test.js
+++ tests/unit/serializers/account-test.js
@@ -1,15 +1,23 @@
+/* global server */
+
 import { moduleForModel, test } from 'ember-qunit';
+import startMirage from '../../helpers/start-mirage';
 
 moduleForModel('account', 'Unit | Serializer | account', {
-  // Specify the other units that are required for this test.
-  needs: ['serializer:account']
+  needs: [ 'serializer:account', 'model:user', 'model:driver' ],
+  beforeEach() {
+    startMirage( this.container );
+  },
+  afterEach() {
+    window.server.shutdown();
+  }
 });
 
-// Replace this with your real tests.
-test('it serializes records', function(assert) {
-  let record = this.subject();
+test('it serializes array responses', function(assert) {
+  assert.expect(1);
+  server.createList( 'account', 3 );
 
-  let serializedRecord = record.serialize();
-
-  assert.ok(serializedRecord);
+  return this.store().findAll('account').then(accounts => {
+    assert.equal( accounts.get('length'), 3, 'three accounts should be loaded' );
+  });
 });
--- tests/unit/serializers/user-test.js
+++ tests/unit/serializers/user-test.js
@@ -1,15 +1,28 @@
+/* global server */
+
+import Ember from 'ember';
 import { moduleForModel, test } from 'ember-qunit';
+import startMirage from '../../helpers/start-mirage';
 
 moduleForModel('user', 'Unit | Serializer | user', {
-  // Specify the other units that are required for this test.
-  needs: ['serializer:user']
+  needs: [ 'serializer:user', 'model:account' ],
+  beforeEach() {
+    startMirage( this.container );
+  },
+  afterEach() {
+    window.server.shutdown();
+  }
 });
 
-// Replace this with your real tests.
-test('it serializes records', function(assert) {
-  let record = this.subject();
+test('it serializes the /users/current response', function(assert) {
+  assert.expect(2);
+  const response = { id: 1, firstName: 'John' };
+  server.get( '/users/current', response, 200 );
 
-  let serializedRecord = record.serialize();
-
-  assert.ok(serializedRecord);
+  Ember.run(() => {
+    return this.store().find('user', 'current').then(user => {
+      assert.equal( user.get('id'),        response.id );
+      assert.equal( user.get('firstName'), response.firstName );
+    });
+  });
 });
