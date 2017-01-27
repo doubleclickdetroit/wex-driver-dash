e60fcbb5425e3c1e69702427764f457a6918c178
Ben Babics <Ben.Babics@wexinc.com>
2017-01-27 16:20:06 -0500

    mirage serialize embeded accounts

--- mirage/serializers/user.js
+++ mirage/serializers/user.js
@@ -1,5 +1,18 @@
 import ApplicationSerializer from './application';
 
 export default ApplicationSerializer.extend({
-  attrs: [ 'id', 'firstName', 'email', 'username', 'onlineApplication', 'company', 'billingCompany' ]
+
+  serialize(user, request) {
+    let json = ApplicationSerializer.prototype.serialize.apply( this, arguments );
+
+    const serialize = ApplicationSerializer.prototype.serialize;
+    json.company        = serialize.apply( this, [ user.company, request ] );
+    json.billingCompany = serialize.apply( this, [ user.billingCompany, request ] );
+
+    delete json.companyId;
+    delete json.billingCompanyId;
+
+    return json;
+  }
+
 });
