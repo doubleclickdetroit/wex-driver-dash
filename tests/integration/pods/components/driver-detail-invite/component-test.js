74aed0f58db211f63fc1425975f633c9ea2221b2
Ben Babics <Ben.Babics@wexinc.com>
2017-02-01 12:13:40 -0500

    MOBPMT-1073 updated specs for template logic

--- tests/integration/pods/components/driver-detail-invite/component-test.js
+++ tests/integration/pods/components/driver-detail-invite/component-test.js
@@ -26,12 +26,36 @@ test('when "driver.isInviteCurrent" is false', function(assert) {
   assert.ok(this.$('.uninvited').is(':visible'), 'the emdash should be visible' );
 });
 
-test('when "driver.isInviteCurrent" is true', function(assert) {
-  const driver = Ember.Object.create({ isInviteCurrent: true });
+test('when "driver.hasInvite" is false', function(assert) {
+  const driver = Ember.Object.create({ hasInvite: false });
   this.set( 'driver', driver );
 
   this.render(hbs`{{driver-detail-invite driver}}`);
-  assert.ok( this.$('.invited').is(':visible'), 'the invite status should be visible' );
+  assert.equal( this.$('.uninvited').length, 1, 'an emdash should be visible' );
+});
+
+test('when "driver.hasInvite" is true', function(assert) {
+  const driver = Ember.Object.create({ hasInvite: true });
+  this.set( 'driver', driver );
+
+  this.render(hbs`{{driver-detail-invite driver}}`);
+  assert.equal( this.$('.uninvited').length, 0, 'an emdash is not visible' );
+});
+
+test('when "driver.isUndeliverable" is true', function(assert) {
+  const driver = Ember.Object.create({ hasInvite: true, isUndeliverable: true });
+  this.set( 'driver', driver );
+
+  this.render(hbs`{{driver-detail-invite driver}}`);
+  assert.equal( this.$('.status').text().trim(), 'Undeliverable', 'status should read "Undeliverable"' );
+});
+
+test('when "driver.isInviteCurrent" is true', function(assert) {
+  const driver = Ember.Object.create({ hasInvite: true, isInviteCurrent: true });
+  this.set( 'driver', driver );
+
+  this.render(hbs`{{driver-detail-invite driver}}`);
+  assert.equal( this.$('.status').text().trim(), 'Invite Sent', 'status should read "Invite Sent"' );
 });
 
 test('when "driver.isSaving" is true', function(assert) {
